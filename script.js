e99755e3295e090ae4edb6de1043a95ecbb27ef9
Carlos Eduardo <silvacarlosoliveira@outlook.com>
2021-07-29 12:30:15 -0300

    Revisado bug de scroll final

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
 const back = document.querySelector('#back');
 const forward = document.querySelector('#forward');
 const items = document.querySelector('#items');
+const item = document.querySelectorAll('.item');
 let count = 0
 
 forward.addEventListener('click', (event) => {
-  if(count > 6200) count = -900
-  count += 900;
+  count+=300;
+  items.scrollBy(300, 0);
+  if (count > 300 * (item.length -1)) {
+    for(var i=0; i<item.length; i++) {
+      items.scrollBy(-300, 0)
+    }
+  count = 0;
+  }
+
   console.log(count);
-  items.scrollTo(count, 0)
 })
 
 back.addEventListener('click', (event) => {
-  if(count < 200) count = 7200
-  count -= 900;
+  count-=300;
+  items.scrollBy(-300, 0)
+  if (count < 0) {
+    count = 300 * (item.length -1);
+    for(var i=0; i<item.length; i++) {
+      items.scrollBy(300, 0)
+    }
+  }
+
   console.log(count);
-  items.scrollTo(count, 0)
 })
 
 // Scroll com a roda do mouse
 items.addEventListener("wheel", (event) => {
   if(event.deltaY < 0){
+    count-=300;
     event.target.scrollBy(300, 0)
   }
   else{
+    count+=300;
     event.target.scrollBy(-300, 0)
   }
 })
